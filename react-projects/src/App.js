6460f8cf0f212dbf8c819bc43252d7a816a0305a
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-08-18 17:43:14 -0700

    Make a small landing page

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import logo from './assets/logo.svg';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        
+        <h1> Welcome to my world</h1>
       </header>
     </div>
   );
 }
 
-export default App;
+
+export function Page()  {
+  return (
+    <div className = "page-content">
+      <img className= 'logo' src={logo} alt= 'logo' />
+      <h1>Fun fact about React</h1>
+      <ul className='list'>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+   
+  )
+} 
+
