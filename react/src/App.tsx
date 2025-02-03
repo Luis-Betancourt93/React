e25ecf7c97b723e001e8befe056371fe63fc7bbe
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-02-02 20:09:04 -0800

    Practice react

---
+++ react/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+    </>
+  )
+}
+
+export default App
